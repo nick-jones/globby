1e5c4a49a5111655f03b4bba2a7afe6e32cf68e6
Nicholas Jones <me@nicholasjon.es>
2014-02-04 10:42:39 +0000

    Error exceptions for the Builder and Tokenizer namespaces

---
+++ lib/Globby/Builder/BuildException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Globby\Builder;
+
+/**
+ * This exception should be thrown if the token translation stage encounters any issues.
+ *
+ * @package Globby\Builder
+ */
+class BuildException extends \RuntimeException { }
---
+++ lib/Globby/Tokenizer/TokenizeException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Globby\Tokenizer;
+
+/**
+ * This exception should be thrown when a Tokenizer implementing class experiences issues during the parsing stage.
+ *
+ * @package Globby\Tokenizer
+ */
+class TokenizeException extends \RuntimeException { }
