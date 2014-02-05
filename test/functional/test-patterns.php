c532e2b4af6d825435d5307b31b317c825c8a1aa
Nicholas Jones <me@nicholasjon.es>
2014-02-05 13:29:37 +0000

    The test-patterns helper script is now more flexible.
    The script will run tests for all test/functional/patterns_*.json files when no arguments
    are supplied. Additionally, the pattern matching has been shifted into a seperate function;
    this affords simple swapping out of the matching backend, allowing others to test their
    own implementations.

--- test/functional/test-patterns.php
+++ test/functional/test-patterns.php
@@ -1,12 +1,24 @@
 <?php
 
-require_once __DIR__ . '/../../vendor/autoload.php';
+error_reporting(E_ALL);
 
-use Globby\Pattern;
+/**
+ * Define the "wildcard_match" function, if undefined. This provides a mechanism for people to test their own
+ * implementation of glob-style wildcard matching, if they so wish. To do so, simply define "wildcard_match" before
+ * including this file, and then call suite() with a path to the appropriate file.
+ */
+if (!function_exists('wildcard_match')) {
+    require_once __DIR__ . '/../../vendor/autoload.php';
 
-if ($argc !== 2 || !is_readable($argv[1])) {
-    echo sprintf("Usage: %s [path]\n", $argv[0]);
-    exit(1);
+    /**
+     * @param string $pattern
+     * @param string $value
+     * @return bool
+     */
+    function wildcard_match($pattern, $value) {
+        $globby = new \Globby\Pattern($pattern);
+        return $globby->match($value);
+    }
 }
 
 /**
@@ -25,37 +37,30 @@ if ($argc !== 2 || !is_readable($argv[1])) {
  */
 function test(array $patterns) {
     foreach ($patterns as $pattern => $values) {
-        $globby = new Pattern($pattern);
-
-        foreach (array_shift($values) as $positive) {
-            validate($globby, $positive, TRUE);
+        foreach ($values[0] as $positive) {
+            validate($pattern, $positive, TRUE);
         }
 
-        foreach (array_shift($values) as $negative) {
-            validate($globby, $negative, FALSE);
+        foreach ($values[1] as $negative) {
+            validate($pattern, $negative, FALSE);
         }
     }
 }
 
 /**
- * @param Pattern $pattern
+ * @param string $pattern
  * @param string $value
  * @param bool $expected
  * @throws \UnexpectedValueException
  */
-function validate(Pattern $pattern, $value, $expected) {
-    $result = $pattern->match($value);
+function validate($pattern, $value, $expected) {
+    $result = wildcard_match($pattern, $value);
 
     if ($result !== $expected) {
         echo 'F';
 
-        $message = sprintf(
-            'Pattern "%s" %s match value "%s". Compiled expression is: "%s"',
-            $pattern->getPattern(),
-            $expected === FALSE ? 'should not' : 'should',
-            $value,
-            $pattern->getRegex()
-        );
+        $verb = $expected === FALSE ? 'should not' : 'should';
+        $message = sprintf('Pattern "%s" %s match value "%s".', $pattern, $verb, $value);
 
         throw new \UnexpectedValueException($message);
     }
@@ -63,13 +68,38 @@ function validate(Pattern $pattern, $value, $expected) {
     echo '.';
 }
 
-$patterns = json_decode(file_get_contents($argv[1]), true);
+/**
+ * @param array $paths
+ */
+function testPaths(array $paths) {
+    foreach ($paths as $path) {
+        $patterns = json_decode(file_get_contents($path), true);
+        echo "\n\n{$path}:\n";
+        test($patterns);
+    }
+}
 
-try {
-    test($patterns);
-    echo "\n\nOK!\n";
+/**
+ * @param array $paths
+ */
+function run(array $paths) {
+    try {
+        echo "Globby Tests";
+        testPaths($paths);
+        echo "\n\nOK! All passed.\n";
+    }
+    catch (Exception $e) {
+        echo "\n\n{$e}\n\nFailed.\n";
+        exit(1);
+    }
 }
-catch (Exception $e) {
-    echo "\n\n{$e}\n";
-    exit(1);
+
+if ($argc > 1) {
+    array_shift($argv);
+    $files = $argv;
 }
+else {
+    $files = glob(__DIR__ . '/patterns_*.json');
+}
+
+run($files);
